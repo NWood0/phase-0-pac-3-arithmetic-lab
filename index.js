e8070b9a99a4d3f87517c12fcb211228d3022e54
Nazire <nazirej@gmail.com>
2022-08-15 18:29:14 -0400

    commit tree f8f015d01f9f1fd0ddb102c89faff8c72ce8a8f5

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n){
+n++ 
+return n
+}
+function decrement(n){
+    n--
+    return n
+}
+function makeInt(n) {
+   let number = parseInt(n, 10)
+    return number
+}
+function preserveDecimal(n) {
+    let number = parseFloat(n);
+    return number
+}
